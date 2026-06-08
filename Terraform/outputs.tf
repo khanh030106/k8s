8eda1c5f8595cda3bc6ff6b2da2b36fd7b8e5f5a
khanh030106 <phamgiakhanh03012006@gmail.com>
2026-06-08 22:37:18 +0700

    Khanhhh

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -3,5 +3,11 @@ output "ec2_public_ip" {
 }
 
 output "alb_dns_name" {
-  value = aws_lb.app_alb.dns_name
+  description = "Public DNS name of the Application Load Balancer"
+  value       = aws_lb.app_alb.dns_name
+}
+
+output "alb_url" {
+  description = "HTTP URL of the Application Load Balancer"
+  value       = "http://${aws_lb.app_alb.dns_name}"
 }
